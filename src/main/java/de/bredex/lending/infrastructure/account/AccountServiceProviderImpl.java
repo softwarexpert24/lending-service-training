ab3cbe73378e0457ca00988d5155f43bed147a6c
Timo Rohrberg <timo.rohrberg.external@gropyus.com>
2022-05-15 10:21:52 +0200

    Improved error handling when accessing other services.

--- src/main/java/de/bredex/lending/infrastructure/account/AccountServiceProviderImpl.java
+++ src/main/java/de/bredex/lending/infrastructure/account/AccountServiceProviderImpl.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import de.bredex.lending.domain.spi.AccountServiceProvider;
@@ -26,8 +27,13 @@ public class AccountServiceProviderImpl implements AccountServiceProvider {
     @Override
     public boolean accountExists(String accountNumber) {
 	final URI accountServiceUri = discoveryClient.getInstances("account-service").get(0).getUri();
-	final ResponseEntity<String> response = restTemplate.getForEntity(accountServiceUri + "/api/v1/account/" + accountNumber,
-		String.class);
-	return response.getStatusCode() == HttpStatus.OK;
+
+	try {
+	    final ResponseEntity<String> response = restTemplate
+		    .getForEntity(accountServiceUri + "/api/v1/account/" + accountNumber, String.class);
+	    return response.getStatusCode() == HttpStatus.OK;
+	} catch (final RestClientException exception) {
+	    return false;
+	}
     }
 }
--- src/main/java/de/bredex/lending/infrastructure/inventory/InventoryServiceProviderImpl.java
+++ src/main/java/de/bredex/lending/infrastructure/inventory/InventoryServiceProviderImpl.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import de.bredex.lending.domain.spi.InventoryServiceProvider;
@@ -18,15 +19,21 @@ public class InventoryServiceProviderImpl implements InventoryServiceProvider {
     private DiscoveryClient discoveryClient;
 
     private final RestTemplate restTemplate;
-    
+
     public InventoryServiceProviderImpl(final RestTemplate restTemplate) {
 	this.restTemplate = restTemplate;
     }
-    
+
     @Override
     public boolean bookExists(String isbn) {
 	final URI inventoryServiceUri = discoveryClient.getInstances("inventory-service").get(0).getUri();
-	final ResponseEntity<String> response = restTemplate.getForEntity(inventoryServiceUri + "/api/v1/inventory/" + isbn, String.class);
-	return response.getStatusCode() == HttpStatus.OK;
+
+	try {
+	    final ResponseEntity<String> response = restTemplate
+		    .getForEntity(inventoryServiceUri + "/api/v1/inventory/" + isbn, String.class);
+	    return response.getStatusCode() == HttpStatus.OK;
+	} catch (final RestClientException exception) {
+	    return false;
+	}
     }
 }
