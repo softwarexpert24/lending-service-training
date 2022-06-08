28153fc07ee5eaf3e1f6453aff64497bcd813942
Timo Rohrberg <timo.rohrberg.external@gropyus.com>
2022-06-08 16:34:07 +0200

    Added scheduled caching of accounts.

--- src/main/java/de/bredex/lending/infrastructure/account/AccountServiceProviderImpl.java
+++ src/main/java/de/bredex/lending/infrastructure/account/AccountServiceProviderImpl.java
@@ -1,14 +1,20 @@
 package de.bredex.lending.infrastructure.account;
 
 import java.net.URI;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import de.bredex.lending.domain.spi.AccountServiceProvider;
@@ -24,28 +30,60 @@ public class AccountServiceProviderImpl implements AccountServiceProvider {
 
     private final RestTemplate restTemplate;
 
+    private final Set<String> accountNumbers = new HashSet<>();
+
     public AccountServiceProviderImpl(final RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
     public boolean accountExists(String accountNumber) {
-        final URI accountServiceBaseUri = resolveAccountServiceBaseUri();
+        Optional<URI> uri = resolveAccountServiceBaseUri();
+        if (uri.isPresent()) {
+            try {
+                final ResponseEntity<String> response = restTemplate.getForEntity(
+                    uri.get() + "/api/v1/account/" + accountNumber, String.class);
 
-        try {
-            final ResponseEntity<String> response = restTemplate
-                .getForEntity(accountServiceBaseUri + "/api/v1/account/" + accountNumber, String.class);
-            return response.getStatusCode() == HttpStatus.OK;
-        } catch (final RestClientException exception) {
-            return false;
+                return response.getStatusCode() == HttpStatus.OK;
+            } catch (ResourceAccessException e) {
+                // Do nothing.
+            }
+        }
+
+        return accountNumbers.contains(accountNumber);
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void updateAccounts() {
+        Optional<URI> uri = resolveAccountServiceBaseUri();
+        if (uri.isPresent()) {
+            try {
+                final ResponseEntity<AccountDto[]> response = restTemplate.getForEntity(
+                    uri.get() + "/api/v1/account",
+                    AccountDto[].class);
+
+                if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
+                    accountNumbers.clear();
+                    for (AccountDto account : response.getBody()) {
+                        accountNumbers.add(account.getNumber());
+                    }
+                }
+            } catch (ResourceAccessException e) {
+                // Do nothing.
+            }
         }
     }
 
-    private URI resolveAccountServiceBaseUri() {
+    private Optional<URI> resolveAccountServiceBaseUri() {
         if (externalServiceBaseUri != null && !externalServiceBaseUri.isEmpty()) {
-            return URI.create(externalServiceBaseUri);
+            return Optional.of(URI.create(externalServiceBaseUri));
         } else {
-            return discoveryClient.getInstances("account-service").get(0).getUri();
+            List<ServiceInstance> instances = discoveryClient.getInstances("account-service");
+            if (!instances.isEmpty()) {
+                return Optional.of(instances.get(0).getUri());
+            } else {
+                return Optional.empty();
+            }
         }
     }
 }
